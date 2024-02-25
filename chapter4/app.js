4e5767d5b32b84b1b70742dabe665c74fb6183f1
MukarramRizvi <imranmaster20@gmail.com>
2024-02-25 21:16:07 +0500

    add folder and 2 files

---
+++ chapter4/app.js
@@ -0,0 +1,3 @@
+document.write(
+  "<big><b>Rules for naming JS variables</b></big> <br><br> Variable names can only contain , number , $ and _ . for example : $my_1stVariable <br>Variable must begin with a letter , $ or _ . for example : $name,_name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords"
+);
